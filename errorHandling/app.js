2420693533e69717cc1e8470f38af6bb8fa05c0d
beberiche <woaol@naver.com>
2021-10-02 21:03:25 +0900

    TIL

---
+++ errorHandling/app.js
@@ -0,0 +1,90 @@
+const path = require('path');
+
+const express = require('express');
+const errorController = require('./controllers/error');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://beberiche:970409@cluster0.kr8nl.mongodb.net/shop?retryWrites=true&w=majority';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
+
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+      // console.log(`log in was success. hello ${user.name}😀`);
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(3000);
+    console.log('server is running....');
+  })
+  .catch((err) => console.log(err));
---
+++ errorHandling/routes/admin.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const express = require('express');
+const { body } = require('express-validator');
+
+const adminController = require('../controllers/admin');
+
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+
+router.use('/', isAuth);
+
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post(
+  '/add-product',
+  [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 5, max: 400 }),
+  ],
+  adminController.postAddProduct
+);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post(
+  '/edit-product',
+  [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 5, max: 400 }),
+  ],
+  adminController.postEditProduct
+);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
