9dd1c4c15683580d8955263fc77c59513e711e01
beberiche <woaol@naver.com>
2021-10-06 21:55:13 +0900

    TIL

--- rest-api/app.js
+++ rest-api/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -50,12 +51,14 @@ app.use(
 app.use(`${__dirname}/images`, express.static(path.join(__dirname, 'images')));
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 const MONGODB_URI =
--- rest-api/routes/feed.js
+++ rest-api/routes/feed.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controller/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
@@ -18,6 +19,17 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
