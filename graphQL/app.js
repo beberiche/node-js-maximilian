7bdc251bda463c1dc33c2981d5b5eac32c628d37
beberiche <woaol@naver.com>
2021-10-17 00:06:34 +0900

    TIL

--- graphQL/app.js
+++ graphQL/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,6 +8,7 @@ const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -109,9 +109,3 @@ mongoose
     app.listen(8080);
   })
   .catch((err) => console.log(err));
-
-const clearImage = (filePath) => {
-  console.log(filePath);
-  filePath = path.join(__dirname, '.', filePath);
-  fs.unlink(filePath, (err) => console.log(err));
-};
--- graphQL/graphql/schema.js
+++ graphQL/graphql/schema.js
@@ -46,13 +46,13 @@ module.exports = buildSchema(`
         login(email: String!, password: String!): AuthData!
         posts(page: Int): PostData!
         post(id: ID!) : Post!
-
     }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
         createPost(postInput: PostInputData): Post!
         updatePost(id:ID!, postInput : PostInputData) : Post!
+        deletePost(id:ID!) : Boolean
     }
 
     schema {
